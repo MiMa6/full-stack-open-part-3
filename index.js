f5c16e2ccf3f2b968a673fe1c82d377b0af6de41
MM <michele.makiki@gmail.com>
2023-12-11 13:05:15 +0200

    phonebook backend step5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     {
@@ -59,6 +60,21 @@ app.delete('/api/persons/:id', (request, response) => {
     
     response.status(204).end()
 })
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    const person = {
+        id: Math.floor(Math.random() * 10000),
+        name: body.name,
+        number: body.number
+    
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
 
 const PORT = 3001
 app.listen(PORT)
