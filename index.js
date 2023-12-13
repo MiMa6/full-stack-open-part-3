17194e4f2b1f9d0df577d9cc21a478a0df12a824
MM <michele.makiki@gmail.com>
2023-12-13 19:31:28 +0200

    Fix conflicts

--- index.js
+++ index.js
@@ -62,49 +62,41 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number } = request.body
+  const { name, number } = request.body
 
-    Person.findByIdAndUpdate(
-        request.params.id,
-        { name, number },
-        { new: true, runValidators: true, context: 'query' }
-    )
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (body.number === undefined) {
-        console.log('number is missing')
-        return response.status(400).json({ error: 'number is missing' })
-    }
+  if (body.number === undefined) {
+    console.log('number is missing')
+    return response.status(400).json({ error: 'number is missing' })
+  }
 
-    if (body.number === undefined) {
-        console.log('number is missing')
-        return response.status(400).json({ error: 'number is missing' })
-    }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
+  if (body.number === undefined) {
+    console.log('number is missing')
+    return response.status(400).json({ error: 'number is missing' })
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   })
-
   person.save()
     .then(savedPerson => {
       response.json(savedPerson)
     })
     .catch(error => next(error))
-
 })
 
 const unknownEndpoint = (request, response) => {
@@ -116,12 +108,12 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 
 app.use(errorHandler)
